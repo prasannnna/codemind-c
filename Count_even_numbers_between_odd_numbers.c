caf48be0ac766f3f3556cbcc940cf73abfc38316
prasannnna <147472476+prasannnna@users.noreply.github.com>
2023-12-27 22:46:48 +0530

    #include<stdio.h> int main() {     int n,cnt=0;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=1;i<n;i++)     {         if(arr[i+1]%2!=0 && arr[i-1]%2!=0 && arr[i]%2==0)         {             cnt++;         }     }     printf("%d",cnt); }

---
+++ Count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,cnt=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i+1]%2!=0 && arr[i-1]%2!=0 && arr[i]%2==0)
+        {
+            cnt++;
+        }
+    }
+    printf("%d",cnt);
+}
